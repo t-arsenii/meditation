87c5edd42040686c82c815b473648e76824b5c3c
KatrychAlina <katricalina5@gmail.com>
2023-10-16 16:46:23 +0200

    added files to client

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
-  import { Routes, Route } from 'react-router-dom'
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import MyComponent from './pages/Homepage/homepage';
+import Hello from './pages/Hello/hello'
+import Registration from './pages/Registration/registration';
 
 function App() {
   return (
-
-        <Routes>
-
-            </Routes>
-
+    <Router>
+      <Routes> {}
+        <Route path="/" element={<MyComponent />} />
+        <Route path="/hello" element={<Hello />} />
+        <Route path="/registration" element={<Registration />} />
+      </Routes>
+    </Router>
   );
 }
 
